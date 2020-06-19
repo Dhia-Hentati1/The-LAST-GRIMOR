9c67e875291a2790050bd61d66da6fc343660f76
1Seichan1 <66797794+1Seichan1@users.noreply.github.com>
2020-06-19 09:39:49 +0200

    Add files via upload

---
+++ vie.c
@@ -0,0 +1,31 @@
+#include"vie.h"
+
+void VIE_Init(Vie *v){
+	v->hp = 200;
+	v->r.x = 25;
+	v->r.y = 15;
+	v->r.w = 225;
+	v->r.h = 30;
+
+	v->bar = SDL_CreateRGBSurface(0,225,30,32,0,0,0,0);
+
+	SDL_FillRect(v->bar, NULL, 0x00ff00);
+
+}
+
+void VIE_ReduireHp(Vie *v, int degat){
+	v->hp -= degat;
+	v->r.w = v->hp;
+}
+
+void VIE_AjouterHp(Vie *v, int degat){
+	v->hp += degat;
+	if(v->hp > 200){
+		v->hp = 200;
+	}
+	v->r.w = v->hp;
+}
+
+void VIE_Render(Vie *v, SDL_Surface **screen){
+	SDL_BlitSurface(v->bar,&v->r,*screen,&v->r);
+}
